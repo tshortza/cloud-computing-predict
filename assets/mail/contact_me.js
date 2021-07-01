1602732595344ff8db0d254e8138528c91c3cc8d
tshortza <61078839+tshortza@users.noreply.github.com>
2021-07-01 20:34:40 +0200

    tony contact

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://sk6ojfxd4i.execute-api.eu-west-1.amazonaws.com/mylambdafunction",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
